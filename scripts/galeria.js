5a67d77f7af504d0b6e1c882d11e5ceb0dc827b3
Vinicius N Barbosa <82734962+ViniciusNB@users.noreply.github.com>
2021-10-05 23:34:02 -0300

    Update galeria.js

--- scripts/galeria.js
+++ scripts/galeria.js
@@ -4,9 +4,7 @@ function pageScroll() {
     window.scrollBy(0,1);
     scrolldelay = setTimeout(pageScroll, 10);
 }
-function pageNotScroll() {
-    
-}
+
 function Redir() {
     window.location.href = "http://viniciusnb.github.io/5months";
 }
