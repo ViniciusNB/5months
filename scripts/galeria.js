3d4d6442ba1eaa22b35577f8b34e5adb4a73a672
ViniciusNB <vsn.barbosa@gmail.com>
2021-10-05 23:21:08 -0300

    Adição de página nova

--- scripts/galeria.js
+++ scripts/galeria.js
@@ -1,3 +1,12 @@
+//pageScroll();
+
+function pageScroll() {
+    window.scrollBy(0,1);
+    scrolldelay = setTimeout(pageScroll, 10);
+}
+function pageNotScroll() {
+    
+}
 function Redir() {
     window.location.href = "http://viniciusnb.github.io/5months";
 }
